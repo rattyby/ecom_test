c2f2b5bcedf2b187e2c7f8d53bfa598eab36c224
Петр Кравцов <rat.f.br@gmail.com>
2022-08-26 16:09:05 +0300

    parsers changed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from prodparser import ProductParser
 
 if __name__ == '__main__':
     good = ProductParser()
-    # details = good.parse('https://www.wildberries.ru/catalog/18256273/detail.aspx?targetUrl=MI')
-    # details = good.parse('https://ozon.ru/product/ruchnaya-espresso-kofevarka-staresso-mini-324093592/?sh=IqdCzDwezw')
+    # details = good.parse('https://www.wildberries.ru/catalog/18166576/detail.aspx?targetUrl=WR')
+    details = good.parse('https://ozon.ru/product/ruchnaya-espresso-kofevarka-staresso-mini-324093592/?sh=IqdCzDwezw')
     # details = good.parse('https://www.ozon.ru/product/kapsulnaya-kofemashina-krups-piccolo-xs-krasnyy-178842724/?sh=yr-vOjksrA')
-    details = good.parse('https://market.yandex.ru/product--kvadrokopter-dji-mavic-air-2-fly-more-combo/673833009')
+    # details = good.parse('https://market.yandex.by/product--karta-pamiati-kingmax-sdhc-class-4/1561108?slug=karta-pamiati-kingmax-sdhc-class-4&productId=1561108&cpc=St02_avah7Cnf0g2VCiyh7vnv9AObTN6S_fK6x0192BWc8a0H7v5yL1g8xdZcxV_q-BxUSiCOMkWZb3VLsjou6NZgCDGOTYM4G_dQtQJEcggZx1HZ9hxdYh_CFsYR7ZA87gRtJb1Q3PcNIHVTU6abhRQQdDuFyNs_koqhOZ1hXnnzYdkUzlEgQ%2C%2C&show-uid=16613238073980432050516000&sku=101282624520&lr=157')
--- prodparser.py
+++ prodparser.py
@@ -1,4 +1,5 @@
 import requests as rq
+import re
 import json
 from selenium import webdriver
 from selenium.common.exceptions import InvalidArgumentException, NoSuchElementException
@@ -11,41 +12,72 @@ class ProductParser:
     HEADERS = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 '
                       'Safari/537.36',
-        }
+    }
 
-    def _ozon_parse(self, url: str) -> str:
+    def _ozon_parse(self, url: str) -> dict:
         """ Parser for product page from OZON """
+        res = {}
         item_list = url.split('/')
         i = item_list.index('product')
         item = '/'.join(item_list[i:-1])
-        url_details = f'https://www.ozon.by/api/composer-api.bx/page/json/v2?url=/{item}/?layout_container='\
+        url_details = f'https://www.ozon.by/api/composer-api.bx/page/json/v2?url=/{item}/?layout_container=' \
                       f'pdpPage2column&layout_page_index=2'
-        return url_details
+        resp = rq.get(url_details)
+        res.update(**resp.json())
+        return res
 
-    def _wb_parse(self, url: str) -> str:
+    def _wb_parse(self, url: str) -> dict:
         """ Parser for product page from WildBerries """
+        res = {}
+        driver = webdriver.Chrome('../../chromedriver.exe')
+        driver.get(url)
+        try:
+            elem = driver.find_element(by=By.XPATH, value='//span[contains(@class, "price-block__price")]')
+        except NoSuchElementException:
+            elem = None
+        res['price'] = float(re.sub('[^\d.]*', '', elem.text)) if elem else 'undefined'
+        driver.quit()
+
         item = url.split('/')[-2]
         url_details = f'https://wbx-content-v2.wbstatic.net/ru/{item}.json'
-        return url_details
+        try:
+            resp = rq.get(url_details)
+            res.update(**resp.json())
+        except json.JSONDecodeError as e:
+            return e
+        res['prod_name'] = res.pop('imt_name')
+        return res
 
     def _ym_parse(self, url: str) -> dict:
         """ Parser for product page from Yandex.Market """
         res_dict = {}
         driver = webdriver.Chrome('../../chromedriver.exe')
         driver.get(url)
-        # captcha
-        elem = driver.find_element(by=By.CLASS_NAME, value='CheckboxCaptcha-Button')
-        if elem:
-            # driver.quit()
-            # return {'error': 'too many requests, captcha detected'}
-            elem.submit()
-            time.sleep(25)  # wait for manual enter captcha
+        # captcha, may be more than once
+        while True:
+            try:
+                elem = driver.find_element(by=By.CLASS_NAME, value='CheckboxCaptcha-Button')
+            except NoSuchElementException:
+                break
+            if elem:
+                # driver.quit()
+                # return {'error': 'too many requests, captcha detected'}
+                elem.submit()
+                time.sleep(15)  # wait for manual enter captcha
+            else:
+                break
         # Name of Product
-        elem = driver.find_element(by=By.TAG_NAME, value='h1')
+        try:
+            elem = driver.find_element(by=By.TAG_NAME, value='h1')
+        except NoSuchElementException:
+            elem = None
         res_dict['prod_name'] = elem.text if elem else 'undefined'
         # Price
-        elem = driver.find_element(by=By.XPATH, value='//span[@data-auto="mainPrice"]/span[1]')
-        res_dict['price'] = float(''.join(elem.text.split(' ')))
+        try:
+            elem = driver.find_element(by=By.XPATH, value='//span[@data-auto="mainPrice"]/span[1]')
+        except NoSuchElementException:
+            elem = None
+        res_dict['price'] = float(''.join(elem.text.split(' '))) if elem else 'undefined'
         # Features
         elem = driver.find_elements(by=By.CLASS_NAME, value='_2oLGf')
         if elem:
@@ -68,23 +100,11 @@ class ProductParser:
     def parse(self, url: str) -> dict:
         """ Method get URL with product and returns dict with params """
         parser = self._get_parser(url.split('/')[2])  # domain address
-        url_product = parser(url)
-        if 'yandex' in url:
-            res = url_product
-        else:
-            try:
-                resp = rq.get(url_product)
-                res = resp.json()
-            except json.JSONDecodeError as e:
-                print(e.strerror, e.args)
-                print(resp.text)
-                res = None
-            except OSError as e:
-                print(e.strerror, e.args)
-                res = None
+        product_info = parser(url)
         # with open('res.json', 'w', encoding='UTF-8') as fp:
         #     json.dump(resp, fp, ensure_ascii=False)
+
         # dictionary with details of the product
-        for k, v in res.items():
+        for k, v in product_info.items():
             print(f'{k}: {v}')
-        return res
+        return product_info
