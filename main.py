7f6f9f089af32f946081b6b859f8f750181bfed8
Петр Кравцов <rat.f.br@gmail.com>
2022-08-29 16:29:07 +0300

    stage 1

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from prodparser import ProductParser
+import json
 
 
 if __name__ == '__main__':
     good = ProductParser()
     # details = good.parse('https://www.wildberries.ru/catalog/18166576/detail.aspx?targetUrl=WR')
-    details = good.parse('https://ozon.ru/product/ruchnaya-espresso-kofevarka-staresso-mini-324093592/?sh=IqdCzDwezw')
-    # details = good.parse('https://www.ozon.ru/product/kapsulnaya-kofemashina-krups-piccolo-xs-krasnyy-178842724/?sh=yr-vOjksrA')
+    # details = good.parse('https://ozon.ru/product/ruchnaya-espresso-kofevarka-staresso-mini-324093592/?sh=IqdCzDwezw')
+    details = good.parse('https://www.ozon.ru/product/kapsulnaya-kofemashina-krups-piccolo-xs-krasnyy-178842724/?sh=yr-vOjksrA')
     # details = good.parse('https://market.yandex.by/product--karta-pamiati-kingmax-sdhc-class-4/1561108?slug=karta-pamiati-kingmax-sdhc-class-4&productId=1561108&cpc=St02_avah7Cnf0g2VCiyh7vnv9AObTN6S_fK6x0192BWc8a0H7v5yL1g8xdZcxV_q-BxUSiCOMkWZb3VLsjou6NZgCDGOTYM4G_dQtQJEcggZx1HZ9hxdYh_CFsYR7ZA87gRtJb1Q3PcNIHVTU6abhRQQdDuFyNs_koqhOZ1hXnnzYdkUzlEgQ%2C%2C&show-uid=16613238073980432050516000&sku=101282624520&lr=157')
+    with open('res.json', 'w', encoding='UTF-8') as fp:
+        json.dump(details, fp, ensure_ascii=False)
--- prodparser.py
+++ prodparser.py
@@ -2,33 +2,47 @@ import requests as rq
 import re
 import json
 from selenium import webdriver
-from selenium.common.exceptions import InvalidArgumentException, NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 import time
 
 
 class ProductParser:
-    """ Parser of product pages from OZON.ru, WildBerries.ru, YandexMarket.ru """
-    HEADERS = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 '
-                      'Safari/537.36',
-    }
+    """ Class Parser of product pages from OZON.ru, WildBerries.ru, YandexMarket.ru """
 
     def _ozon_parse(self, url: str) -> dict:
         """ Parser for product page from OZON """
         res = {}
+        # Name and Price are taken from the start url
+        driver = webdriver.Chrome('../../chromedriver.exe')
+        driver.get(url)
+        try:
+            elem = driver.find_element(by=By.XPATH, value='//h1[contains(@class, "ym5")]')
+        except NoSuchElementException:
+            elem = None
+        res['prod_name'] = elem.text if elem else 'undefined'
+        try:
+            elem = driver.find_element(by=By.XPATH, value='//span[contains(@class, "m3x")]')
+        except NoSuchElementException:
+            elem = None
+        res['price'] = float(re.sub('[^\d.]*', '', elem.text)) if elem else 'undefined'
+        driver.quit()
+        # features take from API JSON
         item_list = url.split('/')
         i = item_list.index('product')
         item = '/'.join(item_list[i:-1])
         url_details = f'https://www.ozon.by/api/composer-api.bx/page/json/v2?url=/{item}/?layout_container=' \
                       f'pdpPage2column&layout_page_index=2'
-        resp = rq.get(url_details)
-        res.update(**resp.json())
+        # too complicated structure, pop only features
+        resp = rq.get(url_details).json().pop('widgetStates')
+        key = [k for k in resp.keys() if k.startswith('webCharacteristics')]
+        res['features'] = json.loads(resp[key[0]])
         return res
 
     def _wb_parse(self, url: str) -> dict:
         """ Parser for product page from WildBerries """
         res = {}
+        # Price is taken from the start url
         driver = webdriver.Chrome('../../chromedriver.exe')
         driver.get(url)
         try:
@@ -37,7 +51,7 @@ class ProductParser:
             elem = None
         res['price'] = float(re.sub('[^\d.]*', '', elem.text)) if elem else 'undefined'
         driver.quit()
-
+        # Features and Name are taken from JSON
         item = url.split('/')[-2]
         url_details = f'https://wbx-content-v2.wbstatic.net/ru/{item}.json'
         try:
@@ -50,7 +64,7 @@ class ProductParser:
 
     def _ym_parse(self, url: str) -> dict:
         """ Parser for product page from Yandex.Market """
-        res_dict = {}
+        res = {}
         driver = webdriver.Chrome('../../chromedriver.exe')
         driver.get(url)
         # captcha, may be more than once
@@ -60,8 +74,6 @@ class ProductParser:
             except NoSuchElementException:
                 break
             if elem:
-                # driver.quit()
-                # return {'error': 'too many requests, captcha detected'}
                 elem.submit()
                 time.sleep(15)  # wait for manual enter captcha
             else:
@@ -71,25 +83,25 @@ class ProductParser:
             elem = driver.find_element(by=By.TAG_NAME, value='h1')
         except NoSuchElementException:
             elem = None
-        res_dict['prod_name'] = elem.text if elem else 'undefined'
+        res['prod_name'] = elem.text if elem else 'undefined'
         # Price
         try:
             elem = driver.find_element(by=By.XPATH, value='//span[@data-auto="mainPrice"]/span[1]')
         except NoSuchElementException:
             elem = None
-        res_dict['price'] = float(''.join(elem.text.split(' '))) if elem else 'undefined'
+        res['price'] = float(''.join(elem.text.split(' '))) if elem else 'undefined'
         # Features
         elem = driver.find_elements(by=By.CLASS_NAME, value='_2oLGf')
         if elem:
             for el in elem:
                 k = el.find_element(by=By.CLASS_NAME, value='_2trXG')
                 v = el.find_element(by=By.CLASS_NAME, value='_3M0mF')
-                res_dict[k.text] = v.text
+                res[k.text] = v.text
         driver.quit()
-        return res_dict
+        return res
 
     def _get_parser(self, domain: str):
-        """ Select parser for url """
+        """ Select a parser for the url """
         if 'ozon' in domain:
             return self._ozon_parse
         elif 'wildberries.' in domain:
@@ -98,13 +110,11 @@ class ProductParser:
             return self._ym_parse
 
     def parse(self, url: str) -> dict:
-        """ Method get URL with product and returns dict with params """
+        """ Method gets URL with product and returns dict with params """
         parser = self._get_parser(url.split('/')[2])  # domain address
         product_info = parser(url)
-        # with open('res.json', 'w', encoding='UTF-8') as fp:
-        #     json.dump(resp, fp, ensure_ascii=False)
 
         # dictionary with details of the product
-        for k, v in product_info.items():
-            print(f'{k}: {v}')
+        # for k, v in product_info.items():
+        #     print(f'{k}: {v}')
         return product_info
